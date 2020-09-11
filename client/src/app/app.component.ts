b00316b64d80c02ff0bbb9e181cf2c824942d1e1
g.vladimir <vladimir.gavrilov@intspirit.com>
2020-09-11 16:51:22 +0300

    add preview

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HttpService } from './http.service'
 @Component({
   selector: 'app-root',
@@ -6,13 +6,19 @@ import { HttpService } from './http.service'
   styleUrls: ['./app.component.scss'],
   providers: [HttpService]
 })
+
 export class AppComponent {
   fileToUpload: File = null;
   constructor(private httpService: HttpService){}
+  @ViewChild('fileInput')
+  fileInputVariable;
 
   handleFileInput(files: FileList) {
     this.fileToUpload = files.item(0);
+    this.preview(files)
   }
+  imagePath;
+  imgURL: any;
 
   uploadFileToActivity() {
     this.httpService.postFile(this.fileToUpload).subscribe(data => {},
@@ -20,4 +26,23 @@ export class AppComponent {
       console.log(error);
     });
   }
+
+  clearFileInput(){
+    this.fileInputVariable.nativeElement.value = "";
+    this.fileToUpload = null
+    this.imgURL = null
+  }
+
+  preview(files) {
+    if (files.length === 0)
+      return;
+
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    }
+  }
 }
+
